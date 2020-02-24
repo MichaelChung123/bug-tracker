3f2e88dbdb2f36418ceb0b5c4712be686783b52d
MichaelChung123 <michael.chung1994@gmail.com>
2020-02-24 13:28:03 -0800

    grabbing data from db and putting it on client

--- queries.js
+++ queries.js
@@ -1,4 +1,5 @@
 const Pool = require('pg').Pool
+
 const pool = new Pool({
     user: 'me',
     host: 'localhost',
@@ -6,12 +7,14 @@ const pool = new Pool({
     password: 'password',
     port: 5432,
 })
+
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
+        // console.log(results.rows);
+        response.json(results.rows)
     })
 }
 
--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import Contacts from './components/contacts';
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {
-    testData: []
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      testData: [],
+      users: []
+    }
   }
 
   componentDidMount() {
     fetch('/ping')
       .then(res => res.json())
       .then(data => this.setState({ testData: data }));
+
+    fetch('/users')
+      .then(res => res.json())
+      .then(data => this.setState({ users: data }));
   }
 
   render() {
+    console.log("ping: ", this.state.users);
+    console.log("users: ", this.state.users);
     return (
       <div>
         <h1>Test Data:</h1>
         <ul>
-          {this.state.testData.map(user =>
-            <li key={user.id}>{user.payload}</li>
+          {this.state.testData.map(data =>
+            <li key={data.id}>{data.payload}</li>
+          )}
+        </ul>
+
+        <ul>
+          {this.state.users.map(user =>
+            <li key={user.id}>{user.name}</li>
           )}
         </ul>
       </div>
