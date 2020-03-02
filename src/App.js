37a696baea7a3e1ae17dc07b9bc0463bd390602d
MichaelChung123 <michael.chung1994@gmail.com>
2020-03-02 11:15:45 -0800

    going to check how to add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import Login from './components/Login';
 import SelectDemoUser from './components/SelectDemoUser';
 import AdminHome from './components/admin/index';
 import UserHome from './components/user/index';
+import Dashboard from './components/admin/Dashboard';
+import Projects from './components/admin/Projects';
+import Tickets from './components/admin/Tickets';
+import SideBar from './components/sidebar/Sidebar';
 
 class App extends Component {
   render() {
     return (
       <Router>
+
         <Switch>
           <Route path='/' exact component={Login} />
           <Route path='/demo-user' component={SelectDemoUser} />
           <Route path='/demo/admin/home/index' component={AdminHome} />
           <Route path='/demo/user/home/index' component={UserHome} />
+
+          <Route path='/dashboard' component={Dashboard} />
+          <Route path='/projects' component={Projects} />
+          <Route path='/tickets' component={Tickets} />
         </Switch>
       </Router>
     );
--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SideBar from '../sidebar/Sidebar';
 
 class Index extends Component {
     render() {
         return (
             <div>
+                <SideBar />
+
                 <h1>Admin</h1>
             </div>
         );
