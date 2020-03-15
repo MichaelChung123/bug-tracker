61c60b96eb3ddd241ae3c92cd244754fa04b2f56
MichaelChung123 <michael.chung1994@gmail.com>
2020-03-15 15:46:33 -0700

    created button

--- src/components/admin/projects/Projects.jsx
+++ src/components/admin/projects/Projects.jsx
@@ -15,27 +15,32 @@ class Projects extends Component {
                 {
                     title: 'first project',
                     activeTickets: 10,
-                    desc: 'this is the project 1s description'
+                    desc: 'this is the project 1s description',
+                    count: 1
                 },
                 {
                     title: 'second project',
                     activeTickets: 10,
-                    desc: 'this is the project 2s description'
+                    desc: 'this is the project 2s description',
+                    count: 2
                 },
                 {
                     title: 'third project',
                     activeTickets: 10,
-                    desc: 'this is the project 3s description'
+                    desc: 'this is the project 3s description',
+                    count: 3
                 },
                 {
                     title: 'fourth project',
                     activeTickets: 10,
-                    desc: 'this is the project 4s description'
+                    desc: 'this is the project 4s description',
+                    count: 4
                 },
                 {
                     title: 'fifth project',
                     activeTickets: 10,
-                    desc: 'this is the project 5s description'
+                    desc: 'this is the project 5s description',
+                    count: 5
                 }
             ]
         }
@@ -59,6 +64,9 @@ class Projects extends Component {
                     <Col xs={6} sm={6} md={6} lg={6}>
                         <h1>All Projects</h1>
                     </Col>
+                    <Col xs={6} sm={6} md={6} lg={6} className='create-project'>
+                        <button>Create New <i className='fa fa-plus-circle nav-icon' /></button>
+                    </Col>
                 </Row>
                 <Container fluid='true'>
                     <Row className='project-row'>
@@ -66,9 +74,11 @@ class Projects extends Component {
                             this.state.projects.map((project, key) => {
                                 return (
                                     <ProjectList
+                                        projects={this.state.projects}
                                         title={project.title}
                                         activeTickets={project.activeTickets}
                                         desc={project.desc}
+                                        count={project.count}
                                     />
                                 );
                             })
@@ -83,15 +93,18 @@ class Projects extends Component {
 class ProjectList extends React.Component {
     render() {
         return (
-            <Col xs={3} sm={3} md={3} lg={3} className='project-block'>
+            <Col xs={12} sm={6} md={6} lg={3} className='project-block'>
                 <Col xs={12} sm={12} md={12} lg={12}>
                     <h3>{this.props.title}</h3>
                 </Col>
                 <Col xs={12} sm={12} md={12} lg={12}>
-                    <p>{this.props.activeTickets}</p>
+                    <p><b>{this.props.activeTickets}</b> Active Tickets</p>
                 </Col>
                 <Col xs={12} sm={12} md={12} lg={12}>
                     <p>{this.props.desc}</p>
+                </Col>
+                <Col xs={12} sm={12} md={12} lg={12} className='more-info'>
+                    <a href="/Projects/Details/1" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </Col>
             </Col>
         );
