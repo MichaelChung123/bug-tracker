25d80f36e9297de4a38ceba1cd23cb35bd5922b0
MichaelChung123 <michael.chung1994@gmail.com>
2020-03-15 12:26:33 -0700

    trying to style the blocks

--- src/components/admin/projects/Projects.jsx
+++ src/components/admin/projects/Projects.jsx
@@ -11,7 +11,33 @@ class Projects extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            projects: [
+                {
+                    title: 'first project',
+                    activeTickets: 10,
+                    desc: 'this is the project 1s description'
+                },
+                {
+                    title: 'second project',
+                    activeTickets: 10,
+                    desc: 'this is the project 2s description'
+                },
+                {
+                    title: 'third project',
+                    activeTickets: 10,
+                    desc: 'this is the project 3s description'
+                },
+                {
+                    title: 'fourth project',
+                    activeTickets: 10,
+                    desc: 'this is the project 4s description'
+                },
+                {
+                    title: 'fifth project',
+                    activeTickets: 10,
+                    desc: 'this is the project 5s description'
+                }
+            ]
         }
     }
 
@@ -28,37 +54,48 @@ class Projects extends Component {
 
     render() {
         return (
-            <h1>Projects</h1>
-        )
+            <Container className='all-projects-container'>
+                <Row className='projects-title'>
+                    <Col xs={6} sm={6} md={6} lg={6}>
+                        <h1>All Projects</h1>
+                    </Col>
+                </Row>
+                <Container fluid='true'>
+                    <Row className='project-row'>
+                        {
+                            this.state.projects.map((project, key) => {
+                                return (
+                                    <ProjectList
+                                        title={project.title}
+                                        activeTickets={project.activeTickets}
+                                        desc={project.desc}
+                                    />
+                                );
+                            })
+                        }
+                    </Row>
+                </Container>
+            </Container>
+        );
     }
 }
 
 class ProjectList extends React.Component {
     render() {
-        return
+        return (
+            <Col xs={3} sm={3} md={3} lg={3} className='project-block'>
+                <Col xs={12} sm={12} md={12} lg={12}>
+                    <h3>{this.props.title}</h3>
+                </Col>
+                <Col xs={12} sm={12} md={12} lg={12}>
+                    <p>{this.props.activeTickets}</p>
+                </Col>
+                <Col xs={12} sm={12} md={12} lg={12}>
+                    <p>{this.props.desc}</p>
+                </Col>
+            </Col>
+        );
     }
 }
 
 export default Projects;
-
-// import React from 'react';
-// import SideBar from '../../sidebar/Sidebar';
-// import { Col, Container } from 'react-bootstrap';
-// import '../../../styles/style.css';
-
-// const Projects = () => {
-//     return (
-//         <div>
-//             <Container fluid='true'>
-//                 <Col style={{paddingLeft: '0px'}} lg={3}>
-//                     <SideBar />
-//                 </Col>
-//                 <Col lg={9}>
-//                     <h1>Projects</h1>
-//                 </Col>
-//             </Container>
-//         </div>
-//     );
-// }
-
-// export default Projects;
