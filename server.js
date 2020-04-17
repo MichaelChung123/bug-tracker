accbea6a9541fb213c4844c5688feeed224d676c
MichaelChung123 <michael.chung1994@gmail.com>
2020-04-16 18:44:07 -0700

    test

--- server.js
+++ server.js
@@ -60,10 +60,10 @@ app.post('/ticket/details/upload/attachment/:id', db.uploadFile);
 
 app.post('/admin/projects/edit/:id', db.editProject);
 app.get('/admin/projects/newest', db.getNewestProjectID);
-// app.listen(port, () => {
-//     console.log(`App running on port ${port}.`);
-// })
-app.listen(process.env.PORT || 5000)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+})
 
 
 
@@ -77,68 +77,3 @@ app.listen(process.env.PORT || 5000)
 
 
 
-
-
-// // var multer = require('multer');
-// // var upload = multer();
-
-// // // // for parsing application/json
-// // // app.use(express.json()); 
-
-// // // // for parsing application/x-www-form-urlencoded
-// // // app.use(express.urlencoded({ extended: true })); 
-
-// // // for parsing multipart/form-data
-// // app.use(upload.array()); 
-// // app.use(express.static('public'));
-
-// // app.post('/api/user', function (req, res) {
-// //     console.log(req.body);
-// //     console.log(req.body.username);
-// // });
-
-// // module.exports = app;
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-// const port = 8080
-// const db = require('./queries')
-// const path = require('path');
-
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.use(bodyParser.json())
-// app.use(
-//     bodyParser.urlencoded({
-//         extended: true,
-//     })
-// )
-
-// app.get('/', (request, response) => {
-//     response.json({
-//         info: 'Node.js, Express, and Postgres API'
-//     });
-// });
-
-// app.get('/admin/tickets/all', db.getTickets)
-// app.get('/admin/dashboard', db.getDashboardContent)
-
-// app.post('/admin/projects/all', db.createProject)
-// app.get('/admin/projects/details/:id', db.getProjectByID)
-// app.get('/admin/projects/details/tickets/:id', db.getTicketsByProjectID);
-// app.get('/admin/projects/details/users/:id', db.getUsersByProjectID);
-// app.get('/admin/users', db.getUsers);
-
-// app.post('/admin/projects/details/select/user/:id', db.assignUserToProject);
-// app.get('/admin/user/:id', db.getUserByID);
-// app.put('/admin/projects/details/deselect/user/:id', db.deleteUserFromProject);
-
-// app.get('/projects/all', db.getAllProjects);
-// app.post('/tickets/create', db.createTicket);
-
-// app.get('/admin/tickets/details/:id', db.getTicketByID);
-
-// app.listen(port, () => {
-//     console.log(`App running on port ${port}.`);
-// })
